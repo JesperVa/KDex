76d0215f20372f1b2641bcb05fa992400914c007
Oliver Berg <oliver@berg.moe>
2022-06-12 19:49:57 +0200

    Format after Kotlin code standards

--- src/main/kotlin/se/vallett/kdex/api/client/requests/DefaultRequest.kt
+++ src/main/kotlin/se/vallett/kdex/api/client/requests/DefaultRequest.kt
@@ -1,33 +1,34 @@
 package se.vallett.kdex.api.client.requests
 
-abstract class DefaultRequest(private val parameters : List<Pair<String, String>> ) {
-
-    abstract val defaultEndpoint : String
+abstract class DefaultRequest(private val parameters: List<Pair<String, String>>) {
+    abstract val defaultEndpoint: String
 
     //TODO add configured URL
-    fun getRequest() : String = "https://api.mangadex.org/$defaultEndpoint${buildParameters()}"
+    fun getRequest(): String = "https://api.mangadex.org/$defaultEndpoint${buildParameters()}"
 
-    private fun buildParameters() : String = "?${generateParams()}"
+    private fun buildParameters(): String = "?${generateParams()}"
 
-    private fun generateParams() : String = parameters.joinToString("&") { (header, value) -> "$header=$value" }
+    private fun generateParams(): String = parameters.joinToString("&") { (header, value) -> "$header=$value" }
 
     abstract class DefaultBuilder<out T : DefaultRequest> {
-        protected val queryParams : ArrayList<Pair<String, String>> = ArrayList()
+        protected val queryParams: ArrayList<Pair<String, String>> = ArrayList()
 
-        protected fun addQueryParam(param : String, value : String) {
+        protected fun addQueryParam(param: String, value: String) {
             queryParams.add(Pair(param, value))
         }
 
-        protected fun addQueryParam(param : String, value : Any) {
+        protected fun addQueryParam(param: String, value: Any) {
             queryParams.add(Pair(param, value.toString()))
         }
 
-        protected fun addQueryParam(param : String, value : Enum<*>) { queryParams.add(Pair(param, value.name)) }
-
-        protected fun addQueryParam(param : String, values : Iterable<*>) {
-            values.forEach{value -> queryParams.add(Pair("$param[]", value.toString())) }
+        protected fun addQueryParam(param: String, value: Enum<*>) {
+            queryParams.add(Pair(param, value.name))
         }
 
-        abstract fun build() : T
+        protected fun addQueryParam(param: String, values: Iterable<*>) {
+            values.forEach { value -> queryParams.add(Pair("$param[]", value.toString())) }
+        }
+
+        abstract fun build(): T
     }
 }
--- src/main/kotlin/se/vallett/kdex/api/manga/list/MangaList.kt
+++ src/main/kotlin/se/vallett/kdex/api/manga/list/MangaList.kt
@@ -1,40 +1,65 @@
 package se.vallett.kdex.api.manga.list
 
 import se.vallett.kdex.api.client.requests.DefaultRequest
-import se.vallett.kdex.api.manga.list.paramvalues.ExcludedTagsMode
-import se.vallett.kdex.api.manga.list.paramvalues.IncludedTagsMode
-import se.vallett.kdex.api.manga.list.paramvalues.MangaContentRating
-import se.vallett.kdex.api.manga.list.paramvalues.MangaPublicationDemographic
-import se.vallett.kdex.api.manga.list.paramvalues.MangaStatus
+import se.vallett.kdex.api.manga.list.paramvalues.*
 
 class MangaList(queryParams: List<Pair<String, String>>) : DefaultRequest(queryParams) {
-
     override val defaultEndpoint: String
         get() = "manga"
 
     class Builder : DefaultBuilder<MangaList>() {
-        fun artists(artists : Iterable<String>) = apply { addQueryParam("artists", artists) }
-        fun authors(authors : Iterable<String>) = apply { addQueryParam("authors", authors) }
-        fun availableTranslatedLanguage(availableTranslatedLanguage: Iterable<String>) = apply { addQueryParam("availableTranslatedLanguage", availableTranslatedLanguage) }
-        fun contentRating(contentRating: Iterable<MangaContentRating>) = apply { addQueryParam("contentRating", contentRating) }
+        fun artists(artists: Iterable<String>) = apply { addQueryParam("artists", artists) }
+
+        fun authors(authors: Iterable<String>) = apply { addQueryParam("authors", authors) }
+
+        fun availableTranslatedLanguage(availableTranslatedLanguage: Iterable<String>) =
+            apply { addQueryParam("availableTranslatedLanguage", availableTranslatedLanguage) }
+
+        fun contentRating(contentRating: Iterable<MangaContentRating>) =
+            apply { addQueryParam("contentRating", contentRating) }
+
         fun createdAtSince(createdAtSince: String) = apply { addQueryParam("createdAtSince", createdAtSince) }
-        fun excludedOriginalLanguage(excludedOriginalLanguage: Iterable<String>) = apply { addQueryParam("excludedOriginalLanguage", excludedOriginalLanguage) }
+
+        fun excludedOriginalLanguage(excludedOriginalLanguage: Iterable<String>) =
+            apply { addQueryParam("excludedOriginalLanguage", excludedOriginalLanguage) }
+
         fun excludedTags(excludedTags: Iterable<String>) = apply { addQueryParam("excludedTags", excludedTags) }
-        fun excludedTagsMode(excludedTagsMode: ExcludedTagsMode) = apply { addQueryParam("excludedTagsMode", excludedTagsMode) }
+
+        fun excludedTagsMode(excludedTagsMode: ExcludedTagsMode) =
+            apply { addQueryParam("excludedTagsMode", excludedTagsMode) }
+
         fun includedTags(includedTags: Iterable<String>) = apply { addQueryParam("includedTags", includedTags) }
-        fun includedTagsMode(includedTagsMode: IncludedTagsMode) = apply { addQueryParam("includedTagsMode", includedTagsMode) }
+
+        fun includedTagsMode(includedTagsMode: IncludedTagsMode) =
+            apply { addQueryParam("includedTagsMode", includedTagsMode) }
+
         fun includes(includes: Iterable<String>) = apply { addQueryParam("includes", includes) }
+
         fun group(group: String) = apply { addQueryParam("group", group) }
-        fun hasAvailableChapters(hasAvailableChapters: Boolean) = apply { addQueryParam("hasAvailableChapters", hasAvailableChapters) }
+
+        fun hasAvailableChapters(hasAvailableChapters: Boolean) =
+            apply { addQueryParam("hasAvailableChapters", hasAvailableChapters) }
+
         fun ids(ids: Iterable<String>) = apply { addQueryParam("ids", ids) }
+
         fun limit(limit: Int) = apply { addQueryParam("limit", limit) }
+
         fun offset(offset: Int) = apply { addQueryParam("offset", offset) }
-        fun originalLanguage(originalLanguage: Iterable<String>) = apply { addQueryParam("originalLanguage", originalLanguage) }
-        fun publicationDemographic(mangaPublicationDemographic: Iterable<MangaPublicationDemographic>) = apply { addQueryParam("publicationDemographic", mangaPublicationDemographic) }
+
+        fun originalLanguage(originalLanguage: Iterable<String>) =
+            apply { addQueryParam("originalLanguage", originalLanguage) }
+
+        fun publicationDemographic(mangaPublicationDemographic: Iterable<MangaPublicationDemographic>) =
+            apply { addQueryParam("publicationDemographic", mangaPublicationDemographic) }
+
         fun status(status: Iterable<MangaStatus>) = apply { addQueryParam("status", status) }
+
         fun title(title: String) = apply { addQueryParam("title", title) }
+
         fun updatedAtSince(updatedAtSince: String) = apply { addQueryParam("updatedAtSince", updatedAtSince) }
+
         fun year(year: Int) = apply { addQueryParam("year", year) }
+
         override fun build(): MangaList = MangaList(queryParams)
     }
 }
